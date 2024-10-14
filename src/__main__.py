afefd148347424682be8a1b0f842bbb523625333
King <2175616761@qq.com>
2024-10-14 12:32:06 +0800

    ✨ feat: update

--- src/__main__.py
+++ src/__main__.py
@@ -5,30 +5,43 @@ from src.config import Config
 import asyncio
 logger = logging.getLogger(__name__)
 
+async def control_car(car:Car):
+  car.set_speed(0.5, 0.5)
+  while True:
+    print(f"current state: {car.distance}, {car.in_road}, {car.have_obstacle}")
+    if(car.distance is None or car.in_road is None or car.speed is None):
+      await asyncio.sleep(0.1)
+      continue
+    if (car.distance < 20):
+      car.set_speed(0.5,0)
+    else :
+      car.set_speed(0.5,0.5)
 async def main():
   setup_logging()
   car = Car()
   await car.connect()
-  car.set_speed(0.5, 0.5)
-  print(car.in_road)
-  print(car.distance)
-  print(car.have_obstacle)
-  while True:
-    # 检测ctrl+c信号并让小车停止
-    try:
-      pass
-    except KeyboardInterrupt:
-      break
-    except Exception as e:
-      logger.error(f"{e}")
-    finally:
-      car.set_speed(0, 0)
-      logger.info("Car stop")
-      break
-
+  # task1 = asyncio.create_task(car.update_state())
+  # task2 = asyncio.create_task(control_car(car))
+  # https://docs.python.org/zh-cn/3/library/asyncio-task.html#
+  await asyncio.gather(
+        asyncio.to_thread(car.update_state),
+        control_car(car))
+  # try:
+  #   await task1
+  #   await task2
+  # except KeyboardInterrupt:
+  #   pass
+  # except Exception as e:
+  #   logger.error(f"{e}")
+  # finally:
+  #   car.set_speed(0.0, 0.0)
+  #   task1.done()
+  #   task2.done()
+  #   logger.info("Car stop")
   # TODO more code/func need call
 
 
 if __name__ == "__main__":
   logger.setLevel(Config.LOG_LEVEL)
   asyncio.run(main())
+  print("done")
--- src/car.py
+++ src/car.py
@@ -7,11 +7,25 @@ logger = logging.getLogger(__name__)
 
 
 class Car:
+  '''
+  This is a Car
+
+  @TODO
+  add more docs
+
+  @Example:
+
+  car = Car()
+
+  await car.connect()
+
+  task1 = asyncio.create_task(car.update_state)
+
+  task2 = asyncio.create_task(other_func)
+
+  '''
   COMP_T = float
   def __init__(self):
-    """
-    必须调用connect
-    """
     self.writer: asyncio.StreamWriter | None = None
     self.reader: asyncio.StreamReader | None = None
     self.r_data: CarReceiveProtocol | None = None
@@ -21,10 +35,6 @@ class Car:
     logger.debug(f"Connecting to server {Config.ServerIP}:{Config.ServerPort}")
     self.reader, self.writer = await asyncio.open_connection(
         Config.ServerIP, Config.ServerPort)
-    # 启动一个协程来接收数据
-    task = asyncio.create_task(self.__receive())
-    # asyncio.run(self.__receive())
-    await task
 
   def __send(self, message):
     if self.writer is None:
@@ -37,16 +47,20 @@ class Car:
     self.writer.write(message.encode('utf-8'))
     logger.debug(f"Sent: {message}")
 
+  async def update_state(self):
+    while True:
+      await self.__receive()
+      await asyncio.sleep(100)
+
   async def __receive(self):
     if self.reader is None:
       raise RuntimeError(
           "connection maybe disconnected, check your link to car")
-    while True:
-      logger.debug("Waiting for data")
-      data = await self.reader.readuntil(b'\r\n')
-      data = data.decode('utf-8')
-      logger.debug(f"Received: {data}")
-      self.r_data = CarReceiveProtocol.from_json(data)
+    logger.debug("Waiting for data")
+    data = await self.reader.readuntil(b'\r\n')
+    data = data.decode('utf-8')
+    logger.debug(f"Received: {data}")
+    self.r_data = CarReceiveProtocol.from_json(data)
 
   def __in_range(self, p:COMP_T, l:COMP_T , r:COMP_T) -> bool:
     return l <= p and p <= r
