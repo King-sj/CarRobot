4c3769f6f34b677273670fd1205b66eb98cc91d7
King <2175616761@qq.com>
2024-10-14 13:07:29 +0800

    fix: 不能同时运行的问题

--- src/__main__.py
+++ src/__main__.py
@@ -6,39 +6,27 @@ import asyncio
 logger = logging.getLogger(__name__)
 
 async def control_car(car:Car):
-  car.set_speed(0.5, 0.5)
+  car.set_speed(0.5, -0.5)
   while True:
     print(f"current state: {car.distance}, {car.in_road}, {car.have_obstacle}")
     if(car.distance is None or car.in_road is None or car.speed is None):
       await asyncio.sleep(0.1)
       continue
     if (car.distance < 20):
-      car.set_speed(0.5,0)
+      print("change state")
+      car.set_speed(0.5,0.0)
     else :
-      car.set_speed(0.5,0.5)
+      print("change state")
+      car.set_speed(0.5,-0.5)
+    await asyncio.sleep(0.1)
+
 async def main():
   setup_logging()
   car = Car()
   await car.connect()
-  # task1 = asyncio.create_task(car.update_state())
-  # task2 = asyncio.create_task(control_car(car))
-  # https://docs.python.org/zh-cn/3/library/asyncio-task.html#
   await asyncio.gather(
-        asyncio.to_thread(car.update_state),
+        car.update_state(),
         control_car(car))
-  # try:
-  #   await task1
-  #   await task2
-  # except KeyboardInterrupt:
-  #   pass
-  # except Exception as e:
-  #   logger.error(f"{e}")
-  # finally:
-  #   car.set_speed(0.0, 0.0)
-  #   task1.done()
-  #   task2.done()
-  #   logger.info("Car stop")
-  # TODO more code/func need call
 
 
 if __name__ == "__main__":
--- src/car.py
+++ src/car.py
@@ -2,34 +2,57 @@ import asyncio
 from src.car_protocols import CarSendProtocol, CarReceiveProtocol
 import logging
 from src.config import Config
-from typing import Tuple,TypeVar
+from typing import Tuple, TypeVar
+
 logger = logging.getLogger(__name__)
 
 
 class Car:
   '''
   This is a Car
-
+  @attention
+  ```await asyncio.sleep(0.1)``` need
   @TODO
   add more docs
 
   @Example:
+  ```python
+  async def control_car(car:Car):
+    car.set_speed(0.5, 0.5)
+    while True:
+      print(f"current state: {car.distance}, {car.in_road}, {car.have_obstacle}")
+      if(car.distance is None or car.in_road is None or car.speed is None):
+        await asyncio.sleep(0.1)
+        continue
+      if (car.distance < 20):
+        print("change state")
+        car.set_speed(0.5,0.0)
+      else :
+        print("change state")
+        car.set_speed(0.5,0.5)
+      await asyncio.sleep(0.1)
+  async def main():
+    setup_logging()
+    car = Car()
+    await car.connect()
+    await asyncio.gather(
+          car.update_state(),
+          control_car(car))
 
-  car = Car()
 
-  await car.connect()
-
-  task1 = asyncio.create_task(car.update_state)
-
-  task2 = asyncio.create_task(other_func)
-
+  if __name__ == "__main__":
+    logger.setLevel(Config.LOG_LEVEL)
+    asyncio.run(main())
+    print("done")
+  ```
   '''
   COMP_T = float
+
   def __init__(self):
     self.writer: asyncio.StreamWriter | None = None
     self.reader: asyncio.StreamReader | None = None
     self.r_data: CarReceiveProtocol | None = None
-    self.speed:Tuple[None|float,None|float] = (None,None)
+    self.speed: Tuple[None | float, None | float] = (None, None)
 
   async def connect(self):
     logger.debug(f"Connecting to server {Config.ServerIP}:{Config.ServerPort}")
@@ -48,9 +71,10 @@ class Car:
     logger.debug(f"Sent: {message}")
 
   async def update_state(self):
+    print("begin update")
     while True:
       await self.__receive()
-      await asyncio.sleep(100)
+      # await asyncio.sleep(0.1)
 
   async def __receive(self):
     if self.reader is None:
@@ -62,29 +86,34 @@ class Car:
     logger.debug(f"Received: {data}")
     self.r_data = CarReceiveProtocol.from_json(data)
 
-  def __in_range(self, p:COMP_T, l:COMP_T , r:COMP_T) -> bool:
+  def __in_range(self, p: COMP_T, l: COMP_T, r: COMP_T) -> bool:
     return l <= p and p <= r
-  def set_speed(self, left_speed:float, right_speed:float):
-    if not isinstance(left_speed,float) or not isinstance(right_speed,float):
+
+  def set_speed(self, left_speed: float, right_speed: float):
+    if not isinstance(left_speed, float) or not isinstance(right_speed, float):
       raise TypeError("speed should be float")
-    if not self.__in_range(left_speed,0,1) or not self.__in_range(right_speed,0,1):
+    if not self.__in_range(left_speed, -1, 1) or not self.__in_range(
+        right_speed, -1, 1):
       raise ValueError("speed should between 0.0 to 1.0")
     self.__send(str(CarSendProtocol(left_speed, right_speed)))
+
   @property
   def distance(self):
     if self.r_data is None:
       logger.debug("not receive any data")
       return None
-    return self.r_data.distance.value
+    return float(self.r_data.distance.value)
+
   @property
   def in_road(self):
     if self.r_data is None:
       logger.debug("not receive any data")
       return None
-    return self.r_data.in_road
+    return bool(self.r_data.in_road)
+
   @property
   def have_obstacle(self):
     if self.r_data is None:
       logger.debug("not receive any data")
       return None
-    return self.r_data.have_obstacle
+    return bool(self.r_data.have_obstacle)
